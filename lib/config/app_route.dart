343d74517f675d61271d4f55d673ddce208ba9d4
rhyanfauzan <rhyananiki@gmail.com>
2022-11-03 07:28:47 +0700

    remove some model

--- lib/config/app_route.dart
+++ lib/config/app_route.dart
@@ -1,9 +1,4 @@
 class AppRoute {
-  static const intro = '/intro';
   static const home = '/home';
   static const signin = '/signin';
-  static const detail = '/detail';
-  static const checkout = '/checkout';
-  static const checkoutSuccess = '/checkout_success';
-  static const detailBooking = '/detail_booking';
 }
